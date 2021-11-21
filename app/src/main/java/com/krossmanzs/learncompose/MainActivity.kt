a348c7e9feba65c959396e776987497d65c34461
krossmanzs <radi.adi78@gmail.com>
2021-11-21 16:19:02 +0700

    Part 4

--- app/src/main/java/com/krossmanzs/learncompose/MainActivity.kt
+++ app/src/main/java/com/krossmanzs/learncompose/MainActivity.kt
@@ -1,41 +1,85 @@
 package com.krossmanzs.learncompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .border(5.dp,Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Gray)
-                    .padding(5.dp)
-                    .border(5.dp,Color.Cyan)
-                    .padding(5.dp)
-//                    .requiredWidth(300.dp) // force width
-            ) {
-                Text(text = "Hello", modifier = Modifier.clickable {
-                    Toast.makeText(this@MainActivity,"Anjay",Toast.LENGTH_SHORT).show()
-                })
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "World")
+            val painter = painterResource(id = R.drawable.mountain)
+            val description = "A beautiful mountain, maybe everest?"
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = description
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(150.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 350f
+                )))
+            Text(
+                text = title,
+                modifier = modifier
+                    .padding(5.dp)
+                    .align(Alignment.BottomStart),
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+            )
         }
     }
 }
