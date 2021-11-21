380c838775523b306dece99d60242efee0fc137d
krossmanzs <radi.adi78@gmail.com>
2021-11-21 14:26:03 +0700

    First Commit

---
+++ app/src/main/java/com/krossmanzs/learncompose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.krossmanzs.learncompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.krossmanzs.learncompose.ui.theme.LearnComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "Linux")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LearnComposeTheme {
+        Greeting("Android")
+    }
+}
