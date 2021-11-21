ff4aef604ae3e21c402269cb2328e6c5cfef38e3
krossmanzs <radi.adi78@gmail.com>
2021-11-21 15:11:32 +0700

    Part 2

--- app/src/main/java/com/krossmanzs/learncompose/MainActivity.kt
+++ app/src/main/java/com/krossmanzs/learncompose/MainActivity.kt
@@ -3,29 +3,35 @@ package com.krossmanzs.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.krossmanzs.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Linux")
+            Row(
+                modifier = Modifier
+                    .width(300.dp)
+                    .fillMaxHeight(0.5f)
+                    .background(Color.Green),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Hello")
+                Text(text = "Linux")
+                Text(text = "Hello")
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearnComposeTheme {
-        Greeting("Linux")
     }
 }
