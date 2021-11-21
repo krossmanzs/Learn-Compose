1fab8dc2b5b9271f0e325178fa099a8bd0c9900a
krossmanzs <radi.adi78@gmail.com>
2021-11-21 15:35:20 +0700

    Part 3

--- app/src/main/java/com/krossmanzs/learncompose/MainActivity.kt
+++ app/src/main/java/com/krossmanzs/learncompose/MainActivity.kt
@@ -1,36 +1,40 @@
 package com.krossmanzs.learncompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.krossmanzs.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    .width(300.dp)
+                    .background(Color.Green)
                     .fillMaxHeight(0.5f)
-                    .background(Color.Green),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .fillMaxWidth()
+                    .border(5.dp,Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Gray)
+                    .padding(5.dp)
+                    .border(5.dp,Color.Cyan)
+                    .padding(5.dp)
+//                    .requiredWidth(300.dp) // force width
             ) {
-                Text(text = "Hello")
-                Text(text = "Linux")
-                Text(text = "Hello")
+                Text(text = "Hello", modifier = Modifier.clickable {
+                    Toast.makeText(this@MainActivity,"Anjay",Toast.LENGTH_SHORT).show()
+                })
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "World")
             }
         }
     }
